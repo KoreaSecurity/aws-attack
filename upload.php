eba42334cdc0a4c77f8d8281f18a2ec38d363dc9
Sang-Hoon Choi <csh0052@gmail.com>
2023-07-05 22:53:51 +0900

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 
-<form enctype="multipart/form-data" action="Action.php" method="POST">
+<form enctype="multipart/form-data" action="action.php" method="POST">
     <input type="hidden" name="MAX_FILE_SIZE" value="300000" />
     이 파일을 전송합니다: <input name="userfile" type="file" />
     <input type="submit" value="파일 전송" />
